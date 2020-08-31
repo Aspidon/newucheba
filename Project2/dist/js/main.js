948e96269ebedbfc8268e00002917cef58405736
Alex <aleksandr-stec@mail.ru>
2020-08-31 22:39:28 +0300

    2020-08-31 lesson 53 new method

--- Project2/dist/js/main.js
+++ Project2/dist/js/main.js
@@ -232,10 +232,17 @@ window.addEventListener('DOMContentLoaded', () => {
                 const request = new XMLHttpRequest();
                 request.open('POST', 'server.php');
 
-                request.setRequestHeader('Content-type', 'multipart/form-data');
+                request.setRequestHeader('Content-type', 'application/json');
                 const formData = new FormData(form);
 
-                request.send(formData);
+                let object = {};
+                formData.forEach((item, key) => {
+                    object[key] = item;
+                });
+
+                const json = JSON.stringify(object);
+
+                request.send(json);
 
                 request.addEventListener('load', () => {
                     if (request.status === 200) {
