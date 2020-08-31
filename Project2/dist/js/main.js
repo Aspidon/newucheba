cbafb5769efb8df117ae35c513c0fac9f8f326b0
Aspidon <aleksandr-stec@mail.ru>
2020-08-31 15:30:27 +0300

    2020-08-31 lesson-48 v2

--- Project2/dist/js/main.js
+++ Project2/dist/js/main.js
@@ -155,27 +155,55 @@ window.addEventListener('DOMContentLoaded', () => {
             this.price = price;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
-            this.changeToUAH;
+            this.changeToUAH();
         }
         changeToUAH() {
             this.price = this.price * this.transfer;
         }
         render() {
-            const div = document.createElement("div");
-            div.innerHTML = `
+            const element = document.createElement("div");
+            element.innerHTML = `
                 <div class="menu__item">
-                <img src="img/tabs/vegy.jpg" alt="vegy">
-                <h3 class="menu__item-subtitle">Меню "Фитнес"</h3>
-                <div class="menu__item-descr">Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>229</span> грн/день</div>
-                </div>
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
                 </div>
             `;
+            this.parent.append(element);
         }
     }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        10,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        5,
+        ".menu .container"
+    ).render();
 
 
 
